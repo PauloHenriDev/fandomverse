06610c9dab3fe05c4236151bd33df0b60c5b0b72
Paulo Henrique <paulohenriquerattes@gmail.com>
2025-06-23 17:06:57 -0300

    adiciona shadow no header

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import FamdomCard from "@/components/ui/FamdomCard";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#875CF5]">
       <Header />
-      <div className="flex flex-col items-center bg-[#875CF5] mt-[20px]">
+      <div className="flex flex-col items-center mt-[20px]">
         <h1 className="text-[50px] text-white font-bold">Escolha sua Fandom</h1>
         <p className="text-[20px] text-[#E3DBFC]">Explore comunidades dedicadas aos seus universos favoritos. Conecte-se com outros fãs, compartilhe teorias e celebre suas paixões.</p>
       </div>
