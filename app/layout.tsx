7a2d3c7d4db73a7c1bec14a6b7d4096b14d7ebb6
Paulo Henrique <paulohenriquerattes@gmail.com>
2025-06-23 12:50:59 -0300

    deleta pastas dentro de auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
+      <body antialiased>
           {children}
       </body>
     </html>
