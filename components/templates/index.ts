1b38e3ae3331ba2dd7232ae26a09650341a05ea4
Paulo Henrique <paulohenriquerattes@gmail.com>
2025-06-27 18:30:35 -0300

    deleta arquivos não utilizados

--- components/templates/index.ts
+++ components/templates/index.ts
@@ -1,5 +1,3 @@
 export { default as HeroSection } from './HeroSection';
-export { default as FilterSection } from './FilterSection';
 export { default as CardGrid } from './CardGrid';
-export { default as PageSection } from './PageSection';
 export { default as UserFandomsSection } from './UserFandomsSection'; 
--- components/ui/FandomTemplate.tsx
+++
@@ -1,7 +0,0 @@
-export default function FandomTemplate() {
-  return (
-    <div className="flex flex-col gap-4">
-        <p>text</p>
-    </div>
-  )
-}
